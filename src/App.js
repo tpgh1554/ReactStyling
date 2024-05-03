f32d7fb2c28c25d5d1a0e92cd486232c5bc967c2
Seho <tpgh1554@naver.com>
2024-05-03 15:47:24 +0900

    05.03 실습

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SassComponent from "./Sasscomponent";
+
+function App() {
+  return (
+    <>
+      <SassComponent />
+    </>
+  );
+}
+
+export default App;
